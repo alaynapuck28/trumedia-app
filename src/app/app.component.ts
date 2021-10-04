904fff1b82941966fb22f5c68af7944fabf83cc8
alaynapuck28 <you@example.com>
2021-10-03 19:21:12 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'project-trumedia';
+  title2;
+ 
+  ngOnInit(){
+    if(environment.API_KEY){
+      this.title2 = environment.API_KEY
+    }
+  }
 }
