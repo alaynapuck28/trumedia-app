7132fffa3a1e3bd5d9ee94e8bd6f08c458ccf9f9
alaynapuck28 <you@example.com>
2021-10-03 19:54:42 -0500

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { environment } from '../environments/environment';
+
 
 @Component({
   selector: 'app-root',
@@ -8,11 +8,9 @@ import { environment } from '../environments/environment';
 })
 export class AppComponent implements OnInit{
   title = 'project-trumedia';
-  title2;
+
  
   ngOnInit(){
-    if(environment.API_KEY){
-      this.title2 = environment.API_KEY
-    }
+  
   }
 }
