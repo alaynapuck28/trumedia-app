ffbd1419f3d51fb7cbef6ec4f263d5e75378e51d
alaynapuck28 <you@example.com>
2021-10-07 09:09:20 -0500

    updates

--- server.js
+++ server.js
@@ -7,24 +7,19 @@ console.log(process.env);
 const app = express();
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/project-trumedia'));
 
-app.get('/*', function (request, response) {
+app.get('/players', function (request, response) {
     response.sendFile(path.join(__dirname, '/dist/project-trumedia/index.html'));
 });
 
 
-let apiRouter = express.Router()
 
 // GET /api/users
-apiRouter.get('/apiToken', async (request, response)=> {
+app.get('/apiToken', async (request, response)=> {
 
     const apiKey = process.env.API_KEY
     console.log(apiKey)
@@ -43,14 +38,7 @@ apiRouter.get('/apiToken', async (request, response)=> {
 
 });
 
-// prefix apiRouter with '/api'
-app.use('/api', apiRouter)
 
 
 
-app.use(express.static('./dist/project-trumedia'));
-
-// app.get('/*', function (request, response) {
-//     response.sendFile(path.join(__dirname, '/dist/project-trumedia/index.html'));
-// })
 
