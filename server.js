9e1db3cdc1232291018356cc5478043e54597916
alaynapuck28 <you@example.com>
2021-10-06 19:01:33 -0500

    test

--- server.js
+++ server.js
@@ -3,16 +3,20 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 const fetch = (...args) => import('node-fetch').then(({default: fetch})=> fetch(...args));
-
 console.log(process.env);
 const app = express();
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/project-trumedia'));
 
-app.get('/players', function (request, response) {
+app.get('/*', function (request, response) {
     response.sendFile(path.join(__dirname, '/dist/project-trumedia/index.html'));
 });
 
@@ -35,10 +39,6 @@ app.get('/apiToken', async (request, response)=> {
 
 });
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+
 
 
